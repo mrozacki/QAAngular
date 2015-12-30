b18e3af74f54df9569e5d3272d751a0e48cfa3ea
Maciek Rozacki <m.rozacki@gmail.com>
2015-12-30 22:30:18 +0100

    prompt for meeting name

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 var app = angular.module('QATool', []);
+var meetingName=prompt("What is the name of your meeting?");
+var main= function(){
+  $("h1").text(meetingName);
+};
+$(document).ready(main);
